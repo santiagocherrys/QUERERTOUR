f24d5a6f5b70b4609eafbaab69236e2b166d5a7f
Santiago Echeverry <santiagocherrys@hotmail.com>
2024-04-15 12:42:59 -0500

    Se pone marcador de google

--- pueblos/js/comercioPueblos.js
+++ pueblos/js/comercioPueblos.js
@@ -8,9 +8,9 @@ let infoWindow;
 let markers = [];
 
 //
-//"https://developers.google.com/maps/documentation/javascript/images/marker_green"
-const MARKER_PATH =
-"https://img.icons8.com/2266EE/search";
+const MARKER_PATH = "https://developers.google.com/maps/documentation/javascript/images/marker_green";
+/* const MARKER_PATH =
+  "https://img.icons8.com/2266EE/search"; */
 const hostnameRegexp = new RegExp("^https?://.+?/");
 
 //const jardin = { lat: sitiosInteres['jardin'].latitud[0], lng: sitiosInteres['jardin'].longitud[0] };
